991dd55aeb9c2bd487badd20b70c362c24cd7863
Eric Uldall <ericuldall@gmail.com>
2016-05-23 07:50:38 -0700

    cleaned up data access methods, implemented .call against cb to remove `this` as an arg

--- index.js
+++ index.js
@@ -9,31 +9,55 @@ var SuperModelCouchdb = function(options){
         name: 'couchdb',
         source: {
             find: function(id, cb){
-                var _this = this;
-                request({
-                    method: 'GET',
-                    url: this.data_sources.couchdb._options.protocol + '://' + this.data_sources.couchdb._options.host + ':' + this.data_sources.couchdb._options.port + '/' + this.data_sources.couchdb._options.database + '/' + id
-                }, function(err, res, body){
-                    if( err ){
-                        throw new Error('Request Error: ' + err);
-                    }
-
-                    var data = JSON.parse(body);
-                    var imports = {};
-                    for(var key in data){
-                        if( key.match(/^_/) ){
-                            _this[key] = data[key]; 
-                        }else{
-                            imports[key] = data[key];
-                        }
-                    }
-                    _this.import(imports);
-                    cb(_this);
+                getDocument.call(this, id, function(data){
+                    this.import(data);
+                    cb.call(this);
+                });
+            },
+            save: function(cb){
+                saveDocument.call(this, function(){
+                    cb.call(this);
                 });
             },
             _options: options
         }
     };
 };
+
+function getDocument(id, cb){
+    var _this = this;
+    request({
+        method: 'GET',
+        url: this.data_sources.couchdb._options.protocol + '://' + this.data_sources.couchdb._options.host + ':' + this.data_sources.couchdb._options.port + '/' + this.data_sources.couchdb._options.database + '/' + id
+    }, function(err, res, body){
+        if( err ){
+            throw new Error('Request Error: ' + err);
+        }
+        var data = JSON.parse(body);
+        cb.call(_this, data);
+    });
+}
+
+function saveDocument(cb){
+    var _this = this;
+    request({
+        method: 'PUT',
+        url: this.data_sources.couchdb._options.protocol + '://' + this.data_sources.couchdb._options.host + ':' + this.data_sources.couchdb._options.port + '/' + this.data_sources.couchdb._options.database + (this.get('_id') ? '/' + this.get('_id') : ''),
+        json: this.export()
+    }, function(err, res, body){
+        if( err ){
+            throw new Error('Request Error: ' + err);
+        }
+
+        var data = body;
+        if( data.ok === true ){
+            _this.set('_id', data.id);
+            _this.set('_rev', data.rev);
+            cb.call(_this);
+        }else{
+            throw new Error('CouchDB ' + data.error + ': ' + data.reason);
+        }
+    });
+}
 
 module.exports = SuperModelCouchdb;
